7c9d553dae45d0171f2ac7526294a28db10d27dd
John Callahan <johnwcallahan@gmail.com>
2019-03-12 21:10:40 -0400

    clean up useOpenClose

--- src/hooks/useOpenClose.ts
+++ src/hooks/useOpenClose.ts
@@ -1,29 +1,20 @@
 import * as React from 'react';
 
-interface OpenCloseState {
-  open: boolean;
-  error?: Linode.ApiFieldError[];
-}
-
 export interface OpenClose {
   isOpen: boolean;
   open: () => void;
   close: () => void;
 }
 
-const defaultState = {
-  open: false
-};
+/**
+ *  Simple hook to keep track of open/close state.
+ * @param  initialState {boolean} default value for isOpen
+ */
+export const useOpenClose = (initialState: boolean = false): OpenClose => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(initialState);
 
-export const useOpenClose = (
-  initialState: OpenCloseState = defaultState
-): OpenClose => {
-  const [entity, setEntity] = React.useState<OpenCloseState>(initialState);
-
-  const open = () => setEntity(prevState => ({ ...prevState, open: true }));
-  const close = () => setEntity(prevState => ({ ...prevState, open: false }));
-
-  const isOpen = entity.open;
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
   return { isOpen, open, close };
 };
