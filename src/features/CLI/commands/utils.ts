8ca3d315dfab77e8e5fa00dd9386d39cfe608f28
John Callahan <johnwcallahan@gmail.com>
2019-04-26 18:01:12 -0400

    Add utility functions to print results in terminal
    Two new utility functions: `createAvailableActionsTable` and `createResultsTable`. The former is for creating a "help" menu of sorts, and the latter is to print the results of the API call to the terminal.
    
    Also adds an ambient module declaration for the type-less 'ascii-table' library.

---
+++ src/features/CLI/commands/utils.ts
@@ -0,0 +1,24 @@
+import * as AsciiTable from 'ascii-table';
+
+export const createAvailableActionsTable = (
+  availableActions: [string, string][]
+) => {
+  const table = new AsciiTable();
+
+  table.setHeading('action', 'summary');
+
+  availableActions.forEach(([eachAction, eachSummary]) => {
+    table.addRow(eachAction, eachSummary);
+  });
+
+  return table;
+};
+
+export const createResultsTable = (headers: string[], rows: any[][]) => {
+  const table = new AsciiTable();
+  table.setHeading(...headers);
+  rows.forEach(eachRow => {
+    table.addRow(...eachRow);
+  });
+  return table;
+};
