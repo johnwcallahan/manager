315cf8ed8ca58ebbcc5a7ada96e96425615466a3
Adrien Courdavault <acourdavault@linode.com>
2020-03-12 09:24:55 -0400

    adding some unit test to actionmenuitem

--- packages/manager/src/components/ActionMenu/ActionMenu.test.tsx
+++ packages/manager/src/components/ActionMenu/ActionMenu.test.tsx
@@ -1,7 +1,15 @@
-import { mount } from 'enzyme';
+import { cleanup, fireEvent, waitForElement } from '@testing-library/react';
+import * as jaxe from 'jest-axe';
 import * as React from 'react';
-
+import { renderWithTheme, toPassAxeCheck } from 'src/utilities/testHelpers';
 import { ActionMenu } from './ActionMenu';
+
+jest.setTimeout(10000);
+
+afterEach(cleanup);
+
+expect.extend(toPassAxeCheck);
+expect.extend(jaxe.toHaveNoViolations);
 
 const classes = {
   actionSingleLink: '',
@@ -12,23 +20,55 @@ const classes = {
   menu: ''
 };
 
+const NB_OF_ACTIONS = 3;
 describe('ActionMenu', () => {
   const action = { title: 'whatever', onClick: () => undefined };
   const createActionsMany = (closeMenu: Function) => {
-    return [action, action, action];
+    return Array(NB_OF_ACTIONS).fill(action);
   };
+  const baseProps = {
+    classes,
+    createActions: createActionsMany,
+    ariaLabel: 'label'
+  };
+  describe('should render', () => {
+    it('a disabled button', () => {
+      const { getByRole } = renderWithTheme(
+        <ActionMenu {...baseProps} disabled={true} />
+      );
+      expect(getByRole('button')).toHaveAttribute('disabled');
+    });
 
-  it.skip('should render a menu when provided many or one action(s).', () => {
-    const result = mount(
+    it('an enabled button', () => {
+      const cb = jest.fn();
+      const { getByRole } = renderWithTheme(
+        <ActionMenu {...baseProps} toggleOpenCallback={cb} />
+      );
+      const btn = getByRole('button');
+      expect(btn).not.toHaveAttribute('disabled');
+      fireEvent.click(btn);
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it(`a list of ${NB_OF_ACTIONS} action menuitem`, async () => {
+    const { getByRole, getAllByRole } = renderWithTheme(
+      <ActionMenu {...baseProps} />
+    );
+    fireEvent.click(getByRole('button'));
+    const listItems = await waitForElement(() => getAllByRole('menuitem'));
+    expect(listItems).toHaveLength(NB_OF_ACTIONS);
+  });
+  it('should pass axe check', async () => {
+    const res = renderWithTheme(
       <ActionMenu
         classes={classes}
         createActions={createActionsMany}
         ariaLabel="label"
+        disabled={true}
       />
     );
-    expect(result.find('WithStyles(ActionMenu)')).toHaveLength(1);
-
-    result.find('IconButton').simulate('click');
-    expect(result.find('WithStyles(MenuItem)')).toHaveLength(4);
+    expect(res).toPassAxeCheck();
+    expect(await jaxe.axe(res.container)).toHaveNoViolations();
   });
 });
